8c358c67002b9fab8f75fa9bb7c9178dc026d764
wuzziqq <egemen@kirpigames.com>
2022-06-14 11:27:15 +0300

    Ready to Polish

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -35,7 +35,7 @@ public class Obstacle : MonoBehaviour
         if (!PlayerController.Instance.isFail)
         {
             PlayerController.Instance.male.transform.DOScale(0, 0.1f);
-            PlayerController.Instance.ghost.transform.DOScale(1, 0.1f);
+            PlayerController.Instance.ghost.transform.DOScale(0.1492373f, 0.1f);
             print("Ragdoll");
             var ragdollMale = AkaliPoolManager.Instance.Dequeue<MaleRagdoll>();
             ragdollMale.transform.position = spawn.transform.position + new Vector3(0,0,2);
